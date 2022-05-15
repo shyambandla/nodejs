0d8bb420072d095c65cf822fdbf9f79066ba1d2c
Andrew Lim <andrewlim1984@gmail.com>
2022-05-14 20:56:08 -0700

    redis socket closes

--- main.js
+++ main.js
@@ -62,21 +62,58 @@ httpServer.listen(3000);
 
 
 
+const smartFlowDataBySymbol = {};
+const bidAskLastData = {};
 (async () => {
-
-  const client = redis.createClient({ url:"redis://159.223.127.139:6379" });
+  const client = redis.createClient({ url:"redis://165.227.81.96:6379" });
   client.on('error', err => {
     console.log('Error ' + err);
-});
+  });
   const subscriber = client.duplicate();
 
   await subscriber.connect();
 
   await subscriber.subscribe('shyam', (message) => {
-    console.log(message); // 'message'
-    io_admin.sockets.emit(message.split("@")[0],message.split("@")[1]);
+    const symbol = message.split("@")[0];
+    const data = message.split("@")[1];
+    if (bidAskLastData[symbol]) {
+      bidAskLastData[symbol].push(data);
+    } else {
+      bidAskLastData[symbol] = [data];
+      smartFlowDataBySymbol[symbol] = makeSmartFlowObject();
+    }
+    // io_admin.sockets.emit(symbol,data);
     const now = Date.now(); // Unix timestamp in milliseconds
-console.log( now );
+// console.log( now );
   });
 
 })();
+
+
+setInterval(() => {calculateSmartFlow()}, 1000);
+
+
+const calculateSmartFlow = async () => {
+  console.time('-----------------SMART FLOW-------------------');
+  for (let key in bidAskLastData) {
+    let eqData = bidAskLastData[key];
+    let smartFlowData = smartFlowDataBySymbol[key];
+    let symbol = key;
+    for (let i = 0; i < eqData.length; i++) {
+      let data = eqData[i].split(',');
+      if (['57', '58', '59'].indexOf(data.slice(-1)[0]) > -1) {
+        smartFlowData.dp.push(data);
+        continue;
+      }
+    }
+    await io_admin.sockets.emit(symbol, [[1231234214, 12341234, 12342343, 12341234, 12342341243], [1234124, 12341234, 12341234, 12341234], {}, smartFlowData.dp])
+  }
+  console.timeEnd('-----------------SMART FLOW-------------------');
+}
+
+
+const makeSmartFlowObject = () => {
+  return {
+    dp: [],
+  }
+}
